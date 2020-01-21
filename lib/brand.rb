421e0f0dc4a209bb5171be69393f7453ca9f123d
Samuel Kyprianou <samuelkyprianou@gmail.com>
2020-01-21 13:41:50 +0000

    sams update

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,14 @@
 class Brand < ActiveRecord::Base
     has_many :shoes
+
+    def brand_search
+        puts"Please enter a brand name to get started:"
+        input = gets.chomp
+        brand = Brand.find_by(brand_name: input)
+        shoes = Shoe.find_by(brand_id: brand)
+        puts "Name: #{shoes.shoe_name}"
+        puts "Price: #{shoes.price}"
+        puts "Rarity: #{shoes.rarity}"
+    end
+
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,16 @@
 class Shoe < ActiveRecord::Base
     belongs_to :seller
     belongs_to :brand
+    
+#     def price_range
+#         puts "Please select your price range:"
+#         puts "
+#         1. £1 - 50
+#         2. £51 - 100
+#         3. £101+
+#         "
+#         input = gets.chomp
+#         if input =="1"
+
+#     end
 end
